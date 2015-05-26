95eab40496ad7a826b5d0fd1e1ddae6de6d43622
Chunis Deng <chunchengfh@gmail.com>
2015-05-26 23:09:10 +0800

    add PATH, and improvement to setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 myconf=~/.myconf
+replace_files="bashrc gdbinit gitconfig hgrc vim vimrc xprofile"
+date=`date +%Y%m%d`
 
-if [ -s $myconf ]; then
-	rm -f $myconf
-fi
+echo "Backup config files and create soft link for them..."
+for f in $replace_files; do
+	if [ -f ~/.$f -o -d ~/.$f ]; then
+		echo mv ~/.$f ~/.$f-$date
+		mv ~/.$f ~/.$f-$date
+	fi
+	if [ -s ~/.$f ]; then
+		echo rm -f ~/.$f
+		rm -f ~/.$f
+	fi
 
-ln -s `pwd` $myconf
+	ln -s $myconf/dot_files/$f ~/.$f
+done
+
