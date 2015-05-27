1bd8854c38d4e5e5171bf6c9246be0ab5040d174
Chunis Deng <chunchengfh@gmail.com>
2015-05-27 23:14:59 +0800

    should remove syslink files earlier

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 myconf=~/.myconf
-replace_files="bashrc gdbinit gitconfig hgrc vim vimrc xprofile"
+replace_files="myconf bashrc gdbinit gitconfig hgrc vim vimrc xprofile"
 date=`date +%Y%m%d`
 
 echo "Backup config files and create soft link for them..."
 for f in $replace_files; do
+	if [ -h ~/.$f ]; then
+		echo rm -f ~/.$f
+		rm -f ~/.$f
+	fi
 	if [ -f ~/.$f -o -d ~/.$f ]; then
 		echo mv ~/.$f ~/.$f-$date
 		mv ~/.$f ~/.$f-$date
-	fi
-	if [ -s ~/.$f ]; then
-		echo rm -f ~/.$f
-		rm -f ~/.$f
 	fi
 
 	ln -s $myconf/dot_files/$f ~/.$f
 done
 
+rm -f ~/.myconf
+ln -s `pwd` ~/.myconf
